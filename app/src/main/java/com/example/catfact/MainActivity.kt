a9ba42cda1b809ab554ff2fd6b0e393065d71787
Qarapapax <aldunsar@yahoo.com>
2023-03-11 14:01:48 +0300

    Chronometer added

--- app/src/main/java/com/example/catfact/MainActivity.kt
+++ app/src/main/java/com/example/catfact/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.catfact
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.widget.Chronometer
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.catfact.api.ApiFactory
 import com.example.catfact.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,8 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.button.setOnClickListener {
+
             lifecycleScope.launch {
+
                 binding.tvCatFact.visibility = INVISIBLE
                 load()
                 binding.tvCatFact.visibility = VISIBLE
@@ -26,7 +34,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun chronometer() {
+        binding.chronometer.base = SystemClock.elapsedRealtime()
+        binding.chronometer.start()
+    }
+
     private suspend fun load() {
+        chronometer()
+
         binding.catImageView.visibility = VISIBLE
         binding.button.isEnabled = false
         val retrofit = ApiFactory.getApiService().loadCatFact()
